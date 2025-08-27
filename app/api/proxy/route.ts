b4529d04d9f7f770058c65f6970da308c7efb25d
Andrey Kostiv <akostiv262@gmail.com>
2025-08-27 18:07:17 +0200

    fix: fix pathname and add rewrite to vercel config

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -1,7 +1,7 @@
 export async function POST(request: Request) {
   const url = new URL(request.url);
   url.hostname = "openrouter.ai";
-  url.pathname = url.pathname.replace("/proxy", "");
+  url.pathname = url.pathname.replace("/api/proxy", "");
 
   console.log(`Modified url: ${url.toString()}`);
   const modifiedRequest = new Request(url, request);
