5ccf73b3238e9c6f20b1be1d30efee40bddb4bba
Andrey Kostiv <akostiv262@gmail.com>
2025-08-27 18:05:55 +0200

    feat: add proxy function

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -1,5 +1,11 @@
-export async function GET(request: Request) {
-  const response = await fetch("https://api.vercel.app/products");
-  const products = await response.json();
-  return Response.json(products);
+export async function POST(request: Request) {
+  const url = new URL(request.url);
+  url.hostname = "openrouter.ai";
+  url.pathname = url.pathname.replace("/proxy", "");
+
+  console.log(`Modified url: ${url.toString()}`);
+  const modifiedRequest = new Request(url, request);
+  const response = await fetch(modifiedRequest);
+
+  return new Response(response.body, response);
 }
